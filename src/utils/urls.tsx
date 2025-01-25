4e5d1771f582f7101fecff80a4ebedc50b5c0a8b
Ed Code <edvard.ekstrom@gmail.com>
2025-01-25 17:12:02 +0100

    wrong API :)

--- src/utils/urls.tsx
+++ src/utils/urls.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-// const API_URL = "https://hebisapi.onrender.com/puzzlehub";
-const API_URL = "http://localhost:3000/puzzlehub";
+const API_URL = "https://hebisapi.onrender.com/puzzlehub";
+// const API_URL = "http://localhost:3000/puzzlehub";
 
 export default API_URL;
